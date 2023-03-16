a7d00ee4e6d4e8bd984c8c4c40af7fb32ffe4a7f
nemo123 <nemo24112003357@gmail.com>
2023-03-16 09:28:14 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MovieSearchApp from "./components/MovieSearchApp";
+
+function App() {
+    return (
+        <div>
+            <MovieSearchApp></MovieSearchApp>
+        </div>
+    );
+}
+
+export default App;
